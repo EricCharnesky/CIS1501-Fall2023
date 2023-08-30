0a43bfc87e502f8fad572f2280e0df86d997f6b7
Eric Charnesky <eric.charnesky@careevolution.com>
2023-08-30 13:01:15 -0400

    add bad comments

--- Chapter1/main.py
+++ Chapter1/main.py
@@ -2,4 +2,9 @@ print('Hello world!')
 
 print("happy wednesday!")
 
+# comments are ignored by python
+# this line of code prints 'yay internet' to the screen
+# this is wrong now, ope
+print("My name is Eric!")
+
 print("Yay internet")
