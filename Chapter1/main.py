3783babb6f66eb4048cc63e7865cf56e8ca02ab7
Eric Charnesky <eric.charnesky@careevolution.com>
2023-08-30 13:43:46 -0400

    arithmetic

--- Chapter1/main.py
+++ Chapter1/main.py
@@ -8,3 +8,20 @@ print("happy wednesday!")
 print("My name is Eric!")
 
 print("Yay internet")
+
+dollars_per_hour = float(input("Enter your hourly wage"))
+
+print("If you make $", dollars_per_hour, "/hour you will make $", dollars_per_hour * 40, "in a week")
+print("That makes $", dollars_per_hour*40*52, "per year ( before taxes) !")
+
+first_number = int(input("enter first number"))
+second_number = int(input("enter second number"))
+
+print(first_number, "+", second_number, " = ", first_number + second_number)
+print(first_number, "-", second_number, " = ", first_number - second_number)
+print(first_number, "*", second_number, " = ", first_number * second_number)
+print(first_number, "/", second_number, " = ", first_number / second_number)
+
+# // is for integer division
+# % is for modulus or modulo, the integer remainder
+print(first_number, "/", second_number, " = ", first_number // second_number, "reminder", first_number % second_number)
