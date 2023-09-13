4a2a42d1ec5fc8a24ba6582d451522c78372c307
Eric Charnesky <eric.charnesky@careevolution.com>
2023-09-13 13:37:46 -0400

    added branching

--- Lab0/main.py
+++ Lab0/main.py
@@ -12,9 +12,25 @@ credits_remaining = credits_required_for_degree - credits_completed
 
 print("you have", math.ceil(credits_remaining / average_credits_per_semester ), "semesters left" )
 
-print("If you are taking less than 12 credits per semester, it will cost about $",
-      credits_remaining * COST_PER_CREDIT)
+if average_credits_per_semester < 12:
+      print("If you are taking less than 12 credits per semester, it will cost about $",
+            credits_remaining * COST_PER_CREDIT)
+      print("more code that runs if average is < 12")
+else:
+      print("if you are taking more than 12 credits per semester, it will cost about $",
+            math.floor(credits_remaining / average_credits_per_semester) * COST_PER_SEMESTER_MAX +
+            ( credits_remaining % average_credits_per_semester ) * COST_PER_CREDIT )
 
-print("if you are taking more than 12 credits per semester, it will cost about $",
-      math.floor(credits_remaining / average_credits_per_semester) * COST_PER_SEMESTER_MAX +
-      ( credits_remaining % average_credits_per_semester ) * COST_PER_CREDIT )
+# use .lower() to make the string all lower case
+year_in_school = input("Enter your year in school (Freshman, Sophomore, Junior, Senior)").lower()
+
+if year_in_school == "freshman":
+      print("Welcome to UMD!")
+elif year_in_school == "sophomore":
+      print("Hope you found some cool clubs to join")
+elif year_in_school == "junior":
+      print("Did you have an internship yet?")
+elif year_in_school == "senior":
+      print("You're almost done!")
+else:
+      print("I don't know what you entered")
