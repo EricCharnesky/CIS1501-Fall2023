4eb662e9260faf6c3e1d3567bf7b3cc84b949af0
Eric Charnesky <EricCharnesky@gmail.com>
2023-10-04 13:47:33 -0400

    more functions

--- Chapter6-Functions/main.py
+++ Chapter6-Functions/main.py
@@ -65,6 +65,135 @@ def get_weather():
     return 70, "sunny"
 
 
+
+
+
+
+def print_greeting(name, greeting):
+    """calls the greeting function passed, then prints name"""
+    greeting()
+    print(name)
+
+
+def tired_greeting():
+    print("Hi, I'm tired")
+
+
+def coffee_induced_hyper_greeting():
+    print("Hi there, I'm having a great day, how are you, did you get any coffee?")
+
+value = 0
+value_to_add = 10
+
+def add_ten():
+    value = value_to_add
+    print(value)
+    return value
+
+def upper_case_name(name):
+    name = name.upper()
+    print(name)
+    return name
+
+first_name = "eric"
+print(first_name)
+upper_case_name(first_name)
+first_name = upper_case_name(first_name) # will change where first_name points to a string in memory
+print(first_name)
+
+def add_to_list(some_list):
+    some_list.append(10)
+
+def reassign_list(some_list):
+    some_list = [10, 20, 30]
+    print(some_list)
+
+
+my_list = [1, 2, 3]
+print(my_list)
+add_to_list(my_list)
+print(my_list)
+reassign_list(my_list)
+print(my_list)
+
+
+def int(value):
+    return 10
+
+def randint(first, second):
+    return 10
+
+print(random.randint)
+
+your_number = int(input("Enter your number"))
+print(your_number)
+
+
+another_value = add_ten()
+more_values = add_ten()
+value = add_ten()
+print(add_ten())
+print(value)
+
+
+def citation_generator(author, title, publisher, year):
+    print(f"{author}, {title}, {publisher}, {year}")
+
+citation_generator("Bailey Miller", "Programming in Python 3", "Zyante Inc", "2023")
+
+# optional keyword arguments
+citation_generator(title="Programming in Python 3", year="2023", publisher="Zyante", author="Bailey")
+
+def print_date(year = '2023', month = '10', day = '04'):
+    print(f'{year}-{month}-{day}')
+
+
+print_date('2023', '10', '04')
+
+print_date("2022", "4")
+
+print_date("2020")
+
+print_date(day="05", month="04")
+
+random.randrange(10)
+range(10)
+range(1, 10)
+range(1, 10, 2)
+
+def print_triangle(size, character="*"):
+    for row in range(1, size+1):
+        print(row * character)
+
+print_triangle(10)
+
+print_triangle(10, "&")
+
+print_triangle(5, "#")
+
+tired_greeting()
+coffee_induced_hyper_greeting()
+print_greeting("eric", tired_greeting) # passes the function as an argument
+print_greeting("eric", coffee_induced_hyper_greeting)
+
+random.randint(1, 10)
+
+
+def get_grade_details():
+    return "eric", "A+"
+
+
+name, grade = get_grade_details()
+
+result_tuple = get_grade_details()
+
+my_name = result_tuple[0]
+my_grade = result_tuple[1]
+
+print(name, grade)
+print(my_name, my_grade)
+
+
 day = "monday"
 print(day)
 
@@ -124,4 +253,6 @@ more_items_to_buy = "y"
 while more_items_to_buy == "y":
     get_item(items)
     more_items_to_buy = input("Do you have more items to buy? y/n")
+
+
 
